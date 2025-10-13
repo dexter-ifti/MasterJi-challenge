72f121295483fef5e59af88ba37a82b477909c86
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-10-13 22:03:59 +0530

    datatype revised

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,20 @@
+"use strict" //treat all JS code as newer Version
+
+// alert("Hello") we are using nodejs not browser
+
+
+let name = "taha"
+let age = 21
+let isAdult = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
